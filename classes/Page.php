c0d784028712cfe2256c6be396715a58dc58a0ed
skoh-fley <breachreach@gmail.com>
2012-01-30 10:16:58 -0800

    put the Page class in its own file also set up autoload

---
+++ classes/Page.php
@@ -0,0 +1,10 @@
+<?php
+
+class Page {
+	const Index   = 0;
+	const Members = 1;
+	const Minutes = 2;
+	const Links   = 3;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,7 +1,37 @@
 <?php
 
-require_once('html/header.html');
-require_once('html/index.html');
-require_once('html/footer.html');
+function __autoload($class) {
+	include "classes/$class.php";
+}
+
+if(isset($_GET["members"]))
+	$page = Page::Members;
+elseif(isset($_GET["minutes"]))
+	$page = Page::Minutes;
+elseif(isset($_GET["links"]))
+	$page = Page::Links;
+else // default page
+	$page = Page::Index;
+
+require_once "html/header.html";
+
+switch($page) {
+	
+	case Page::Index:
+		require_once "html/index.html";
+		break;
+	case Page::Members:
+		require_once "html/members.html";
+		break;
+	case Page::Minutes:
+		require_once "html/minutes.html";
+		break;
+	case Page::Links:
+		require_once "html/links.html";
+		break;
+	
+}
+
+require_once "html/footer.html";
 
 ?>
