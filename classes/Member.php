bb971eb3ff46165a8ee829786dd753b95fb3c2e0
skoh-fley <breachreach@gmail.com>
2012-02-01 17:40:52 -0800

    spaces -> tabs

--- classes/Member.php
+++ classes/Member.php
@@ -1,37 +1,38 @@
 <?php
 
 class Member {
-
-  private $name;
-  private $position;
-  private $link;
-
-  public function __construct($name, $position, $link) {
-
-    $this->name = explode("-", $name);
-
-    foreach ($this->name as $key => $lc_name) {
-      $this->name[$key] = ucfirst($lc_name);
-    }
-
-    $this->name = implode(" ", $this->name);
-
-    $this->position = ucfirst($position);
-    $this->link = $link;
-  }
-
-  public function getName() {
-    return $this->name;
-  }
-
-  public function getPosition() {
-    return $this->position;
-  }
-
-  public function getLink() {
-    return $this->link;
-  }
-
+	
+	private $name;
+	private $position;
+	private $link;
+	
+	public function __construct($name, $position, $link) {
+		
+		$this->name = explode("-", $name);
+		
+		foreach ($this->name as $key => $lc_name) {
+			$this->name[$key] = ucfirst($lc_name);
+		}
+		
+		$this->name = implode(" ", $this->name);
+		
+		$this->position = ucfirst($position);
+		$this->link = $link;
+		
+	}
+	
+	public function getName() {
+		return $this->name;
+	}
+	
+	public function getPosition() {
+		return $this->position;
+	}
+	
+	public function getLink() {
+		return $this->link;
+	}
+	
 }
 
 ?>
